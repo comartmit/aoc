e990ffc5b043a3de096608ccc127796780ef6220
Martin Couturier <lego.miego@gmail.com>
2020-12-14 20:35:29 -0500

    day 13

--- 2020/day13/main.go
+++ 2020/day13/main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	start, buses := parse(input)
 	fmt.Printf("part 1: %v \n", part1(start, buses))
+	fmt.Printf("part 2: %v \n", part2(buses))
 }
 
 func part1(start int, buses []string) (t int) {
@@ -33,6 +34,23 @@ func part1(start int, buses []string) (t int) {
 	return bestbid * (best - start)
 }
 
+func part2(bs []string) (t int) {
+	increment := 1
+
+	for i, b := range bs {
+		if b == "x" {
+			continue
+		}
+		bus, _ := strconv.Atoi(b)
+		for ((t+i)%bus) != 0 || t == 0 {
+			t += increment
+		}
+		increment *= bus
+	}
+
+	return t
+}
+
 func parse(input string) (int, []string) {
 	parts := strings.Split(input, "\n")
 	depart, _ := strconv.Atoi(parts[0])
@@ -43,3 +61,6 @@ func parse(input string) (int, []string) {
 
 var input = `1005595
 41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,557,x,29,x,x,x,x,x,x,x,x,x,x,13,x,x,x,17,x,x,x,x,x,23,x,x,x,x,x,x,x,419,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19`
+
+var inputSimple = `11
+7,13,x,x,59,x,31,19`
